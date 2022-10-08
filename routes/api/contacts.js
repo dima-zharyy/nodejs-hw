d3a92a28ab15e1e5971c2559e74ede061033c921
dima-zharyy <zharyydimka@gmail.com>
2022-10-08 17:33:50 +0300

    setup db | added new route

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const { contacts: controller } = require('../../controllers');
+const isValidId = require('../../middlewares/isValidId');
 
 const router = express.Router();
 
 router.get('/', controller.getContacts);
 
-router.get('/:contactId', controller.getContactById);
+router.get('/:contactId', isValidId, controller.getContactById);
 
 router.post('/', controller.addContact);
 
-router.delete('/:contactId', controller.removeContact);
+router.delete('/:contactId', isValidId, controller.removeContact);
 
-router.put('/:contactId', controller.updateContact);
+router.put('/:contactId', isValidId, controller.updateContact);
+
+router.patch(
+  '/:contactId/favorite',
+  isValidId,
+  controller.updateContactFavoriteStatus
+);
 
 module.exports = router;
